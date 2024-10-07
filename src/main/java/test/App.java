5e19615beff6f06590d70529b825253b474a5c61
Scott Schulthess <scottschulthess@gmail.com>
2024-10-07 14:30:38 -0400

    add mirorrd example

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -3,12 +3,23 @@
  */
 package test;
 
+import java.util.Map;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        // Get all environment variables
+        Map<String, String> env = System.getenv();
+
+        // Print each environment variable
+        for (Map.Entry<String, String> entry : env.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
         System.out.println(new App().getGreeting());
     }
 }
